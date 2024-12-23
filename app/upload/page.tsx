97b4733ed7e619405d5857d881c34d80fc29360c
Murat Sert <muratsert1453@gmail.com>
2024-12-23 02:11:50 +0000

    Wrapped useSearchParams in suspense

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
+
 import UserJourney from '@/components/UserJourney';
 
 export default function UploadPage() {
   return (
-    <div>
+    <Suspense>
       <UserJourney />
-    </div>
+    </Suspense>
   );
 }
